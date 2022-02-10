76e99d4dd603dcaa1e50a5a4f08c761344cb77b4
Mohit2322 <parepallymohit@gmail.com>
2022-02-10 16:02:34 +0530

    ValueLabs technical assessment

---
+++ Assessment1/Assessment1/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Assessment1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Microsoft.Extensions.Configuration;
+using System.Text.Json;
+
+namespace Assessment1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration iconfig;
+        public HomeController(ILogger<HomeController> logger,IConfiguration config)
+        {
+            _logger = logger;
+            iconfig = config;
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var jsn = await httpClient.GetStringAsync(iconfig.GetValue("url", ""));
+                var temp = JsonSerializer.Deserialize<Dictionary<string, ClassModel>>(jsn);
+                List<ClassModel> classes = temp.Values.ToList();
+                float maxMath1Val = classes.Max(r => r.Math1);
+                float minMath1Val = classes.Min(r => r.Math1);
+                var tempres = temp.Where(x => x.Value.Math1 == maxMath1Val || x.Value.Math1 == minMath1Val).ToDictionary(x=>x.Key,x=>x.Value);
+                ViewBag.result = JsonSerializer.Serialize(tempres);
+            }
+            return View();
+        }
+
+    }
+}
---
+++ Assessment1/Assessment1/Models/ClassModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assessment1.Models
+{
+    public class ClassModel {
+        public float Math1 { get; set; }
+        public float His { get; set; }
+        public float Geo { get; set; }
+        public float Eng { get; set; }
+    }
+}
